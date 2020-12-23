c5babf32a3c57cd5cb211f064c40b1c2eac1cce9
Miel2000 <fabien.orsn@gmail.com>
2020-12-23 22:10:26 +0100

    DiscountRuleType

--- src/Controller/StoreController.php
+++ src/Controller/StoreController.php
@@ -2,19 +2,36 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\DiscountRule;
+use App\Form\DiscountRuleType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class StoreController extends AbstractController
 {
     /**
      * @Route("/store", name="store")
      */
-    public function index(): Response
+    public function index(EntityManagerInterface $em,Request $request): Response
     {
+        $discountRule = new DiscountRule();
+        $form = $this->createForm(DiscountRuleType::class, $discountRule);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+            $discountRule = $form->getData();
+
+            $em->persist($discountRule);
+            $em->flush();
+        }
+
+
         return $this->render('store/index.html.twig', [
-            'yeet' => 'yeboi'
+            'yeet' => 'yeboi',
+            'form' => $form->createView()
       
         ]);
     }
